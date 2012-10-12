0cf522b379534dc210b0b1ea0e495de69938ea85
User <user@CS225-VM.localdomain>
2012-10-12 00:48:34 -0500

    Update for Python 3.2

--- game.py
+++ game.py
@@ -21,8 +21,8 @@ def game_count(n):
 
 def print_header(i, n):
     for k in range(num_digits(n) - num_digits(i)):
-        print '',
-    print str(i) + ": ",
+        print('', end = '')
+    print(str(i) + ": ", end = '')
 
 
 def print_hist(i, n, max_val):
@@ -30,12 +30,12 @@ def print_hist(i, n, max_val):
 
     # I need this stupid case because of line wrapping
     if (n > 50):
-        print n
+        print(n)
         return
    
     for i in range(n):
-        print 'x',
-    print ' --- ' + str(n)
+        print('x', end = '')
+    print(' --- ' + str(n))
 
 def pattern(n):
     max_val = 0
@@ -44,8 +44,8 @@ def pattern(n):
         print_hist(i, pos, n - 1)
         if (pos > max_val):
             max_val = pos
-    print "\n\n\n"
-    print "max: " + str(max_val)
+    print("\n\n\n")
+    print("max: " + str(max_val))
 
 
 
