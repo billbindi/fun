bf185c189578d5497db14579e05144c9b3a49ea9
Bill Bindi <billdbindi@gmail.com>
2012-10-12 12:29:28 -0500

    Consolodate version problems.

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+from printer import my_print
 
 def num_digits(n):
     if (n//10 == 0):
@@ -21,8 +22,8 @@ def game_count(n):
 
 def print_header(i, n):
     for k in range(num_digits(n) - num_digits(i)):
-        print(' ', end = '')
-    print(str(i) + ": ", end = '')
+        my_print(' ', e = '')
+    my_print(str(i) + ": ", e = '')
 
 
 def print_hist(i, n, max_val):
@@ -30,12 +31,12 @@ def print_hist(i, n, max_val):
 
     # I need this stupid case because of line wrapping
     if (n > 50):
-        print(n)
+        my_print(n)
         return
    
     for i in range(n):
-        print('x', end = '')
-    print(' -- ' + str(n))
+        my_print('x', e = '')
+    my_print(' -- ' + str(n))
 
 def pattern(n):
     max_val = 0
@@ -44,15 +45,15 @@ def pattern(n):
         print_hist(i, pos, n - 1)
         if (pos > max_val):
             max_val = pos
-    print("\n\n\n")
-    print("max: " + str(max_val))
+    my_print('')
+    my_print("max: " + str(max_val))
 
 def game_build(n):
     if (n <= 0):
         return False
     lis = []
     while (n != 1):
-        lis.append(n)
+        lis.append(int(n))
         if (n % 2 == 0):
             n /= 2
         else:
@@ -65,15 +66,15 @@ def game(n):
     steps = len(lis)
     for i in range(steps):
         print_header(i + 1, steps)
-        print(lis[i])
+        my_print(lis[i])
 
 
 def print_usage():
-    print("Usage for the 3n + 1 game")
-    print("         python <file> <type> <num>\n")
-    print("         file - {game.py | game27.py}, game.py for python versions 3 and above, else game27.py")
-    print("         type - {freq | one}, frequency will show hitogram data of the number of steps taken for 1 - num, one will just run it once and show the steps needed for num")
-    print("         num  - either the max value for frequency mode or the value to display for one mode")
+    my_print("Usage for the 3n + 1 game")
+    my_print("         python <file> <type> <num>\n")
+    my_print("         file - {game.py | game27.py}, game.py for python versions 3 and above, else game27.py")
+    my_print("         type - {freq | one}, frequency will show hitogram data of the number of steps taken for 1 - num, one will just run it once and show the steps needed for num")
+    my_print("         num  - either the max value for frequency mode or the value to display for one mode")
 
 
 if (len(sys.argv) <= 2):
---
+++ printer.py
@@ -0,0 +1,13 @@
+import sys
+
+def my_print(val, e = '\n'):
+	'''
+	This function is used to bridge the syntax between different versions of Python.
+	If using a version below 3, delete the first pund symbol here.
+	'''
+
+	#'''
+	print(val, end = e)
+	'''
+	print val, e
+	#'''
